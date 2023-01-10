020b753b5bb07aed99e7a394f1906c0aa9ce51ee
Shrikant-Wankhade <wshrikantp10@gmail.com>
2023-01-10 15:59:47 +0530

    card badge now renders conditionally depending upon event location

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import Navbar from "./components/nav.jsx"
 import Hero from "./components/hero.jsx"
 import Card from "./components/card.jsx"
-import data from "./data.js"
+import data from "./data.jsx"
 
 import './App.css'
 
 function App() {
   const Cards = data.map(card=>{
     return <Card 
+        key={card.id}
         img= {card.coverImg}
         rating={card.stats.rating}
         reviewCount={card.stats.reviewCount}
         country={card.location}
         title={card.title}
         price={card.price}
+        openSpots={card.openSpots}
       />
   })
 
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,20 @@
 import React from "react"
 
-export default function Card({img,rating,reviewCount, country,title,price}) {
+export default function Card({key,img,rating,reviewCount,country,title,price,openSpots}) {
+
+    let badgeText ;
+    if(openSpots === 0){
+        badgeText = "SOLD OUT";
+    }else if(location === "online"){
+        badgeText = "ONLINE";
+    }else {
+        badgeText = country;
+    }
     return (
         <div className="card">
+            <div className="card--status">{badgeText}</div>
             <img src={`../../images/${img}`} className="card__image" ></img>
-            <div className="card--status"></div>
+            
             <div className="card__info">
                 <div className="card__info__rating">
                     <img src="../../images/star.png" className="card__info__rating--star"></img>
